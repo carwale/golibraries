47a79405237ad1e8d2268a4d5ff6c58eea116b42
pratik vasa <pratik.vasa@carwale.com>
2018-04-13 15:06:08 +0530

    adding consul agent and utilites to golibraries

---
+++ goutilities/Utilities.go
@@ -0,0 +1,57 @@
+package goutilities
+
+import (
+	"math/rand"
+	"time"
+)
+
+var r1 *rand.Rand
+
+func init() {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 = rand.New(s1)
+}
+
+//RandomIntBetween gives a randon integer between two numbers
+func RandomIntBetween(a int, b int) int {
+	return r1.Intn(b-a) + a
+}
+
+//RandomInt gives a random int less than the number provided
+func RandomInt(n int) int {
+	return r1.Intn(n)
+}
+
+//RandomInt64 gives a 64bit random number
+func RandomInt64() int64 {
+	return r1.Int63()
+}
+
+//RandomUint64 gives an unsigned 64 bit random number
+func RandomUint64() uint64 {
+	return uint64(rand.Uint32())<<32 + uint64(rand.Uint32())
+}
+
+//RandomString gives a random string of length = parameter
+func RandomString(strlen int) string {
+	const chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+	result := ""
+	for i := 0; i < strlen; i++ {
+		index := r1.Intn(len(chars))
+		result += chars[index : index+1]
+	}
+	return result
+}
+
+//RemoveDuplicates removes duplicates from the slice. It returns a new slice
+func RemoveDuplicates(a []string) []string {
+	result := []string{}
+	seen := map[string]string{}
+	for _, val := range a {
+		if _, ok := seen[val]; !ok {
+			result = append(result, val)
+			seen[val] = val
+		}
+	}
+	return result
+}
