248388a5ab23f882f54f74036c0b7d0058586119
Meet Shah <meet.shah@carwale.com>
2018-08-08 11:29:23 +0530

    Add multisource client to servicediscovery

---
+++ servicediscovery/multisourceclient.go
@@ -0,0 +1,44 @@
+package servicediscovery
+
+import (
+	"fmt"
+)
+
+type multiClient struct {
+	clients []IServiceDiscoveryAgent
+}
+
+//NewMultiSourceClient returns new K8s Service discovery agent
+func NewMultiSourceClient(clients ...IServiceDiscoveryAgent) IServiceDiscoveryAgent {
+	multiclient := &multiClient{
+		clients: clients,
+	}
+	return multiclient
+}
+
+func (m *multiClient) RegisterService(name, ipAddress, port, healthCheckPort string, checkFunction func() (bool, error), isDockerType bool) (string, error) {
+	// not implemented as returning multiple service ids violates interface
+	// to decide whether it is needed
+	return "", nil
+}
+
+func (m *multiClient) DeregisterService(serviceID string) {
+	for _, client := range m.clients {
+		client.DeregisterService(serviceID)
+	}
+}
+
+// GetHealthyServices returns service instances from all clients
+func (m *multiClient) GetHealthyService(moduleName string) ([]string, error) {
+	var endpoints []string
+	for _, client := range m.clients {
+		ep, err := client.GetHealthyService(moduleName)
+		if err == nil {
+			endpoints = append(endpoints, ep...)
+		}
+	}
+	if len(endpoints) == 0 {
+		return nil, fmt.Errorf("No instances found for %s", moduleName)
+	}
+	return endpoints, nil
+}
