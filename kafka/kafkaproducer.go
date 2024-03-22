c9f1cd1b9e503dfda10ffde33fc19278903c03b6
Sarvesh Mishra <sarvesh.m@carwale.com>
2024-03-22 16:29:06 +0530

    update kafka version to 2.0.2 arm support
    Signed-off-by: Sarvesh Mishra <sarvesh.m@carwale.com>

--- kafka/kafkaproducer.go
+++ kafka/kafkaproducer.go
@@ -8,7 +8,7 @@ import (
 	"syscall"
 
 	"github.com/carwale/golibraries/gologger"
-	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
 // Producer carries all the settings for the kafka producer
